551fd804611eeb91813f401790d5c593629a3648
Erica Liu <ericaenjoy3@yahoo.co.uk>
2017-11-20 11:44:58 -0500

    Correcting for a bug

--- R/RNAMethods.R
+++ R/RNAMethods.R
@@ -405,7 +405,7 @@ setMethod(f = "PCAplot",
     if (pca2d) {
       p1 <- ggplot(x) +
       geom_point(aes_(x = ~PC1, y = ~ PC2), size = 5, color = 'grey') +
-      labs(x = paste("PC1 (", ve[1], "of variance", sep = " "), y = paste("PC2 (", ve[2], "of variance", sep = " ")) +
+      labs(x = paste("PC1 (", round(ve[1], digits = 2), "of variance)", sep = " "), y = paste("PC2 (", round(ve[2], digits = 2), digits), "of variance)", sep = " ")) +
       geom_label_repel(
         aes_(x = ~ PC1, y = ~ PC2, fill = obj@grps, label = rownames(x)),
         fontface = 'bold', color = 'white',
